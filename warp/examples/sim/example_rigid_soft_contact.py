f686cd17b63f3516657eb86ae01205640b9cfe3c
Eric Heiden <eheiden@nvidia.com>
2024-11-21 14:28:34 -0800

    Roll back example_rigid_soft_contact.py

--- warp/examples/sim/example_rigid_soft_contact.py
+++ warp/examples/sim/example_rigid_soft_contact.py
@@ -17,11 +17,6 @@ import warp as wp
 import warp.sim
 import warp.sim.render
 
-# wp.config.verify_cuda = True
-# wp.config.verify_fp = True
-
-from tqdm import trange
-
 
 class Example:
     def __init__(self, stage_path="example_rigid_soft_contact.usd"):
@@ -38,11 +33,9 @@ class Example:
         self.profiler = {}
 
         builder = wp.sim.ModelBuilder()
+        builder.default_particle_radius = 0.01
 
-        partial_builder = wp.sim.ModelBuilder()
-        partial_builder.default_particle_radius = 0.01
-
-        partial_builder.add_soft_grid(
+        builder.add_soft_grid(
             pos=wp.vec3(0.0, 0.0, 0.0),
             rot=wp.quat_identity(),
             vel=wp.vec3(0.0, 0.0, 0.0),
@@ -58,23 +51,10 @@ class Example:
             k_damp=0.0,
         )
 
-        b = partial_builder.add_body(origin=wp.transform((0.5, 2.5, 0.5), wp.quat_identity()))
-        partial_builder.add_shape_sphere(body=b, radius=0.75, density=100.0)
+        b = builder.add_body(origin=wp.transform((0.5, 2.5, 0.5), wp.quat_identity()))
+        builder.add_shape_sphere(body=b, radius=0.75, density=100.0)
 
-        num_envs = 50
-
-        root_envs = int(num_envs ** 0.5)
-        for i in range(num_envs):
-            builder.add_builder(
-                partial_builder,
-                xform=wp.transform(
-                    wp.vec3((i // root_envs) * 4.0 - root_envs * 2.0, 0.0, (i % root_envs) * 4.0 - root_envs * 2.0), wp.quat_identity()
-                ),
-            )
-
-        builder.soft_contact_max = 16 * 1024 * num_envs
-
-        self.model = builder.finalize(requires_grad=True)
+        self.model = builder.finalize()
         self.model.ground = True
         self.model.soft_contact_ke = 1.0e3
         self.model.soft_contact_kd = 0.0
@@ -109,7 +89,7 @@ class Example:
             (self.state_0, self.state_1) = (self.state_1, self.state_0)
 
     def step(self):
-        with wp.ScopedTimer("step", dict=self.profiler, active=False):
+        with wp.ScopedTimer("step", dict=self.profiler):
             if self.use_cuda_graph:
                 wp.capture_launch(self.graph)
             else:
@@ -120,7 +100,7 @@ class Example:
         if self.renderer is None:
             return
 
-        with wp.ScopedTimer("render", active=False):
+        with wp.ScopedTimer("render"):
             self.renderer.begin_frame(self.sim_time)
             self.renderer.render(self.state_0)
             self.renderer.end_frame()
@@ -137,14 +117,14 @@ if __name__ == "__main__":
         default="example_rigid_soft_contact.usd",
         help="Path to the output USD file.",
     )
-    parser.add_argument("--num_frames", type=int, default=100, help="Total number of frames.")
+    parser.add_argument("--num_frames", type=int, default=300, help="Total number of frames.")
 
     args = parser.parse_known_args()[0]
 
     with wp.ScopedDevice(args.device):
         example = Example(stage_path=args.stage_path)
 
-        for _ in trange(args.num_frames):
+        for _ in range(args.num_frames):
             example.step()
             example.render()
 
