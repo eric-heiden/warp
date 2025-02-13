Fix nmodel, nenv computation in interop, more input fixes

warp/sim/engine_collision_interop.py
@@ -230,11 +230,18 @@ def collision_jax(
     type_pair_count = np.zeros(n_geom_type_pairs, dtype=np.int32)
 
     nenv = 1
-    for i in range(d.geom_xpos.ndim):
+    for i in range(d.geom_xpos.ndim - 1):
         nenv *= d.geom_xpos.shape[i]
     nenv //= ngeom
     if nenv == 0:
-        raise RuntimeError("Batch size of mjx.Data is 0.")
+        raise RuntimeError("nenv cannot be zero.")
+
+    nmodel = 1
+    for i in range(m.geom_size.ndim - 1):
+        nmodel *= m.geom_size.shape[i]
+    nmodel /= ngeom
+    if nmodel == 0:
+        raise RuntimeError("nmodel cannot be zero.")
 
     # output shapes
     npts = nenv * max_contact_points
@@ -297,13 +304,14 @@ def collision_jax(
         # m.pair_friction,
         # m.pair_solref,
         # m.pair_solimp,
-        convex_vert,
+        convex_vert.reshape((-1, 3)),
         convex_vert_offset,
         type_pair_offset.astype(np.int32),
         type_pair_count,
         int(nenv),
+        int(nmodel),
         int(ngeom),
-        int(m.npair),
+        # int(m.npair),
         int(m.nbody),
         int(m.nexclude),
         int(max_contact_points),